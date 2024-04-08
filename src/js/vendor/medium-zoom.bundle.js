7ec9b6fd2bda1f81897f74e32a2e2a6496b1225a
Saverona <Saverona@bk.ru>
2024-04-08 20:25:53 +0300

    11

--- src/js/vendor/medium-zoom.bundle.js
+++
@@ -1,9 +0,0 @@
-;(function () {
-  var Mzm = (window.mzm = require('medium-zoom/dist/medium-zoom.min'))
-  // 'span.image img'
-  //    css selector that medium-zoom connects for standard images like jpg or png
-  // 'div.imageblock img'
-  //    css selector that medium-zoom connects for svg images
-  // with that, there is no need to define a role in the image for zooming - all images are zoomable !
-  Mzm('div.imageblock img', { background: '#fff', margin: 10 })
-})()
